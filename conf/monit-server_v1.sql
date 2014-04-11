da87c8df67c354102cfb4922c1ba63744a292bb7
wdxxs2z <jackyuan88726@gmail.com>
2014-04-11 09:35:44 +0800

    modify beans

--- conf/monit-server_v1.sql
+++ conf/monit-server_v1.sql
@@ -1,8 +1,8 @@
 /*Drop database*/
-drop database ibank_mmonit;
+drop database mmonit;
 /*Create database mmonit*/
-create database ibank_mmonit;
-use ibank_mmonit;
+create database mmonit;
+use mmonit;
 /*Create table monit critical*/
 create table monit(
 monitId varchar(255) not null primary key,   /*Monit ID monit的唯一标示*/
@@ -11,6 +11,8 @@ monitHostName varchar(32),                   /*monit 安装端的主机名*/
 monitHostIp varchar(42),                     /*monit 安装端的IP*/
 monitHostStatus int,                         /*monit 安装端的主机状态 0：存活 1：未连接*/
 monitHostPort varchar(6),                    /*monit 安装端的接收端口*/
+username varchar(32),                        /*monit 用户名*/
+password varchar(64),                        /*monit 用户密码*/
 updateTime varchar(16),                      /*系统运行时间*/
 platformName varchar(20),                    /*平台名*/
 platformRelease varchar(255),                /*平台内核版本*/
@@ -103,3 +105,308 @@ collected_usec varchar(16),                  /*事件 数据采集持续时间*/
 message varchar(255),                        /*事件具体信息*/
 monitId varchar(255) not null                /*事件关联monit*/
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table monitfile(
+file_id INT(5) UNSIGNED AUTO_INCREMENT NOT NULL primary key,
+fileName varchar(24),
+collected_sec varchar(24),
+collected_usec varchar(24),
+status int,
+filemode int,
+fileuid int,
+filegid int,
+filetimestamp varchar(64),
+filesize varchar(128),
+filechecksum varchar(255),
+monitId varchar(255) not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Create table static_processes*/
+create table statistic_processes (
+statistic_pro_id INT(5) UNSIGNED AUTO_INCREMENT NOT NULL primary key,
+processName varchar(24),
+avg_pro_mempercent numeric(4,1),
+avg_pro_memKilobyte numeric(4,1),
+avg_pro_cpupercent numeric(4,1),
+statistic_type varchar(16),                 /*1m 15m 1hour 12hour 1day 7day*/
+monitId varchar(255) not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+create table statistic_systems (
+statistic_sys_id INT(5) UNSIGNED AUTO_INCREMENT NOT NULL primary key,
+systemName varchar(24),
+avg_sys_load numeric(5,2),
+avg_sys_syscpu numeric(4,1),
+avg_sys_mempercent numeric(4,1),
+avg_sys_memTol numeric(5,0),
+avg_sys_swapTol numeric(5,0),
+statistic_type varchar(16),                 /*15m 1hour 12hour 1day 7day*/
+monitId varchar(255)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*datatype:  1:min 2:15min 3:1hout 4:1day*/
+/*    CPU Usage    */
+create table statistic_cpu_usage (
+statistic_cpu_id INT(5) UNSIGNED AUTO_INCREMENT NOT NULL primary key,
+inserttime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+serverCpuUsage_max numeric(5,1),
+serverCpuUsage_min numeric(5,1),
+serverCpuUsage_avg numeric(5,1),
+datatype int,
+servertype int,
+servermname varchar(64),
+monitId varchar(255) not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*    MEM Usage    */
+create table statistic_mem_usage (
+statistic_mem_id INT(5) UNSIGNED AUTO_INCREMENT NOT NULL primary key,
+inserttime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+serverMemUsage_max numeric(5,1),
+serverMemUsage_min numeric(5,1),
+serverMemUsage_avg numeric(5,1),
+datatype int,
+servertype int,
+servername varchar(64),
+monitId varchar(255) not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*    LOAD Average */
+create table statistic_load_average (
+statistic_load_id INT(5) UNSIGNED AUTO_INCREMENT NOT NULL primary key,
+inserttime timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+systemLoadAverage_max numeric(5,1),
+systemLoadAverage_min numeric(5,1),
+systemLoadAverage_avg numeric(5,1),
+datatype int,
+systemname varchar(64),
+monitId varchar(255)  not null
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Procedure structure for `process15mJob`
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `process15mJob`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `process15mJob`(IN monitid varchar(255),IN pname varchar(64))
+begin
+
+declare pcpuSql varchar(1000);
+declare pmemSql varchar(1000);
+
+set pcpuSql = concat("insert into statistic_process_cpu_15m(processCpuPercenttotal_avg,datatype,processname,monitId) 
+select avg(processCpuPercenttotal),3,processName,monitId 
+from process where monitId='" ,monitid, "' and processName='" ,pname, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 15 minute) and now()");
+
+set pmemSql = concat("insert into statistic_process_mem_15m(processMemPercenttotal_avg,datatype,processname,monitId) 
+select avg(processMemPercenttotal),3,processName,monitId 
+from process where monitId='" ,monitid, "' and processName='" ,pname, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 15 minute) and now()");
+
+
+set @sql1 = pcpuSql;
+set @sql2 = pmemSql;
+
+prepare stmt_pcpu from @sql1;
+prepare stmt_pmem from @sql2;
+
+execute stmt_pcpu;
+execute stmt_pmem;
+end
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Procedure structure for `process1dayJob`
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `process1dayJob`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `process1dayJob`(IN `monitid` varchar(255),IN `pname` varchar(64))
+BEGIN
+
+declare pcpuSql varchar(1000);
+declare pmemSql varchar(1000);
+
+set pcpuSql = concat("insert into statistic_process_cpu_usage(processCpuPercenttotal_max,processCpuPercenttotal_min,processCpuPercenttotal_avg,datatype,processname,monitId) 
+select max(processCpuPercenttotal),min(processCpuPercenttotal),avg(processCpuPercenttotal),2,processName,monitId 
+from process where monitId='" ,monitid, "' and processName='" ,pname, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 day) and now()");
+
+set pmemSql = concat("insert into statistic_process_mem_usage(processMemPercenttotal_max,processMemPercenttotal_min,processMemPercenttotal_avg,datatype,processname,monitId) 
+select max(processMemPercenttotal),min(processMemPercenttotal),avg(processMemPercenttotal),2,processName,monitId 
+from process where monitId='" ,monitid, "' and processName='" ,pname, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 day) and now()");
+
+
+set @sql1 = pcpuSql;
+set @sql2 = pmemSql;
+
+prepare stmt_pcpu from @sql1;
+prepare stmt_pmem from @sql2;
+
+execute stmt_pcpu;
+execute stmt_pmem;
+
+END
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Procedure structure for `process1hourJob`
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `process1hourJob`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `process1hourJob`(IN `monitid` varchar(255),IN `pname` varchar(64))
+BEGIN
+
+declare pcpuSql varchar(1000);
+declare pmemSql varchar(1000);
+
+set pcpuSql = concat("insert into statistic_process_cpu_usage(processCpuPercenttotal_max,processCpuPercenttotal_min,processCpuPercenttotal_avg,datatype,processname,monitId) 
+select max(processCpuPercenttotal),min(processCpuPercenttotal),avg(processCpuPercenttotal),1,processName,monitId 
+from process where monitId='" ,monitid, "' and processName='" ,pname, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 hour) and now()");
+
+set pmemSql = concat("insert into statistic_process_mem_usage(processMemPercenttotal_max,processMemPercenttotal_min,processMemPercenttotal_avg,datatype,processname,monitId) 
+select max(processMemPercenttotal),min(processMemPercenttotal),avg(processMemPercenttotal),1,processName,monitId 
+from process where monitId='" ,monitid, "' and processName='" ,pname, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 hour) and now()");
+
+
+set @sql1 = pcpuSql;
+set @sql2 = pmemSql;
+
+prepare stmt_pcpu from @sql1;
+prepare stmt_pmem from @sql2;
+
+execute stmt_pcpu;
+execute stmt_pmem;
+
+END
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Procedure structure for `system15mJob`
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `system15mJob`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `system15mJob`(IN monitid varchar(255))
+begin 
+declare cpuSql varchar(1000);
+declare memSql varchar(1000);
+declare loadSql varchar(1000);
+
+set cpuSql = concat("insert into statistic_cpu_15m(systemCpuUsage_avg,userCpuUsage_avg,waitCpuUsage_avg,datatype,systemname,monitId) 
+select avg(systemSysCpu),avg(systemUserCpu),avg(systemWaitCpu),3,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 15 minute) and now()");
+
+set memSql = concat("insert into statistic_mem_15m(systemMemperc_avg,datatype,systemname,monitId) 
+select avg(systemMemperc),3,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 15 minute) and now()");
+
+set loadSql = concat("insert into statistic_load_15m(loadAvg_avg,datatype,systemname,monitId) 
+select avg(loadAvg01),3,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 15 minute) and now()");
+
+
+set @sql1 = cpuSql;
+set @sql2 = memSql;
+set @sql3 = loadSql;
+
+prepare stmt_cpu from @sql1;
+prepare stmt_mem from @sql2;
+prepare stmt_load from @sql3;
+
+execute stmt_cpu;
+execute stmt_mem;
+execute stmt_load;
+ 
+end
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Procedure structure for `system1dayJob`
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `system1dayJob`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `system1dayJob`(IN `monitid` varchar(255))
+begin 
+declare cpuSql varchar(1000);
+declare memSql varchar(1000);
+declare loadSql varchar(1000);
+
+set cpuSql = concat("insert into statistic_cpu_usage(systemCpuUsage_max,systemCpuUsage_min,systemCpuUsage_avg,userCpuUsage_max,
+userCpuUsage_min,userCpuUsage_avg,waitCpuUsage_max,waitCpuUsage_min,waitCpuUsage_avg,datatype,systemname,monitId) 
+select max(systemSysCpu),min(systemSysCpu),avg(systemSysCpu),max(systemUserCpu),min(systemUserCpu),avg(systemUserCpu),
+max(systemWaitCpu),min(systemWaitCpu),avg(systemWaitCpu),2,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 day) and now()");
+
+set memSql = concat("insert into statistic_mem_usage(systemMemperc_max,systemMemperc_min,systemMemperc_avg,datatype,systemname,monitId) 
+select max(systemMemperc),min(systemMemperc),avg(systemMemperc),2,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 day) and now()");
+
+set loadSql = concat("insert into statistic_load_average(loadAvg_max,loadAvg_min,loadAvg_avg,datatype,systemname,monitId)  
+select max(loadAvg01),min(loadAvg01),avg(loadAvg01),2,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 day) and now()");
+
+
+set @sql1 = cpuSql;
+set @sql2 = memSql;
+set @sql3 = loadSql;
+
+prepare stmt_cpu from @sql1;
+prepare stmt_mem from @sql2;
+prepare stmt_load from @sql3;
+
+execute stmt_cpu;
+execute stmt_mem;
+execute stmt_load;
+
+end
+;;
+DELIMITER ;
+
+-- ----------------------------
+-- Procedure structure for `system1hourJob`
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `system1hourJob`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `system1hourJob`(IN `monitid` varchar(255))
+begin 
+declare cpuSql varchar(1000);
+declare memSql varchar(1000);
+declare loadSql varchar(1000);
+
+set cpuSql = concat("insert into statistic_cpu_usage(systemCpuUsage_max,systemCpuUsage_min,systemCpuUsage_avg,userCpuUsage_max,
+userCpuUsage_min,userCpuUsage_avg,waitCpuUsage_max,waitCpuUsage_min,waitCpuUsage_avg,datatype,systemname,monitId) 
+select max(systemSysCpu),min(systemSysCpu),avg(systemSysCpu),max(systemUserCpu),min(systemUserCpu),avg(systemUserCpu),
+max(systemWaitCpu),min(systemWaitCpu),avg(systemWaitCpu),1,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 hour) and now()");
+
+set memSql = concat("insert into statistic_mem_usage(systemMemperc_max,systemMemperc_min,systemMemperc_avg,datatype,systemname,monitId) 
+select max(systemMemperc),min(systemMemperc),avg(systemMemperc),1,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 hour) and now()");
+
+set loadSql = concat("insert into statistic_load_average(loadAvg_max,loadAvg_min,loadAvg_avg,datatype,systemname,monitId)  
+select max(loadAvg01),min(loadAvg01),avg(loadAvg01),1,systemname,monitId from system where monitId='", monitid, 
+"' and FROM_UNIXTIME(collected_sec) between subdate(now(), interval 1 hour) and now()");
+
+
+set @sql1 = cpuSql;
+set @sql2 = memSql;
+set @sql3 = loadSql;
+
+prepare stmt_cpu from @sql1;
+prepare stmt_mem from @sql2;
+prepare stmt_load from @sql3;
+
+execute stmt_cpu;
+execute stmt_mem;
+execute stmt_load;
+
+end
+;;
+DELIMITER ;
